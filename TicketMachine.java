87d21f45217570be3c74552af7134e6d09b94a96
jorgevff <jvalladaresfontano@gmail.com>
2017-10-12 22:59:41 +0200

    Se implementa metodo que muestra el precio

--- TicketMachine.java
+++ TicketMachine.java
@@ -18,6 +18,8 @@ public class TicketMachine
     // The total amount of money collected by this machine.
     private int total;
 
+    private int status;
+
     /**
      * Create a machine that issues tickets of the given price.
      * Note that the price must be greater than zero, and there
@@ -28,6 +30,7 @@ public class TicketMachine
         price = cost;
         balance = 0;
         total = 0;
+        status = 0;
     }
 
     /**
@@ -35,7 +38,7 @@ public class TicketMachine
      */
     public int getPrice()
     {
-        return price;
+        return price; 
     }
 
     /**
@@ -55,11 +58,17 @@ public class TicketMachine
         balance = balance + amount;
     }
 
+
     /**
      * Print a ticket.
      * Update the total collected and
      * reduce the balance to zero.
      */
+    public void prompt()
+    {
+        System.out.println("Please insert the correct amount of money.");
+    }
+
     public void printTicket()
     {
         // Simulate the printing of a ticket.
@@ -74,5 +83,18 @@ public class TicketMachine
         total = total + balance;
         // Clear the balance.
         balance = 0;
+    }
+
+    /**
+     * devuelve el total
+     */
+    public int getTotal()
+    {
+        return total;
+    }
+    
+    public void showPrice()
+    {
+       System.out.println("The price of a ticket is " + price + " cents."); 
     }
 }
